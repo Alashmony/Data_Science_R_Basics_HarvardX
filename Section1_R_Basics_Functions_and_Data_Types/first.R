30d07d82188bb6fb6e0ac0a6c2a0d596d674288d
alashmony <a.alashmony@outlook.com>
2024-02-29 16:51:43 +0200

    Finished the first section

--- Section1_R_Basics_Functions_and_Data_Types/first.R
+++ Section1_R_Basics_Functions_and_Data_Types/first.R
@@ -22,6 +22,7 @@ b
 ls()
 '''
 #Functions
+'''
 ls
 help(ls)
 help("ls")
@@ -32,3 +33,38 @@ help("+")
 ?"+"
 data()
 Co2
+'''
+#Data Types
+class(ls)
+class(CO2)
+class(1)
+
+data("murders")
+str(murders)
+head(murders)
+murders$population
+names(murders)
+pop <- murders$population
+length(pop)
+length(murders$population)
+
+a <- murders$abb
+length(a)
+
+b <- murders[["abb"]]
+identical(a,b)
+
+# We can see the class of the region variable using class
+class(murders$region)
+
+# Determine the number of regions included in this variable 
+length(levels(murders$region))
+
+'''Tables
+The function table takes a vector as input and returns the frequency of each unique element in the vector.
+We will use the table function to answer this question.
+Use the table function in one line of code to create a table showing the number of states per region.
+'''
+# Write one line of code to show the number of states per region
+table(murders[["region"]])
+
